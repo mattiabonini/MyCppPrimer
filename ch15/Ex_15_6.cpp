bb286221b4dc4cbb55e775d4bda64bca97c8afce
mattiabonini <boninimattia96@gmail.com>
2023-02-24 10:45:44 -0500

    Refactoring with abstract class

--- ch15/Ex_15_6.cpp
+++ ch15/Ex_15_6.cpp
@@ -26,17 +26,13 @@ class Quote {
 	std::string bookNo;
 };
 
-class Bulk_quote : public Quote {
+class Disc_quote : public Quote {
 	public:
-	Bulk_quote() = default;
-	Bulk_quote(const std::string &id, double sales_price, size_t qty, double dis) :
+	Disc_quote() = default;
+	Disc_quote(const std::string &id, double sales_price, size_t qty, double dis) :
 		Quote(id, sales_price), min_qty(qty), discount(dis) {}
 	
-	double net_price(size_t n) const override { // If I don't declare this virtual, is the polymorphism handled dynamically? YES! -> Once it is declared virtual it is always virtual
-		if(n >= min_qty)
-			return n * price * (1 - discount);
-		else return n*price;
-	}
+	double net_price(size_t n) const = 0;
 	
 	std::ostream& debug(std::ostream& os = std::cout) override {
 		Quote::debug() << ", min_quantity: " << min_qty << ", discount: " << discount*100 << "%";
@@ -46,27 +42,43 @@ class Bulk_quote : public Quote {
 	protected:
 	size_t min_qty = 0;
 	double discount = 0.0;
-
 };
 
-class Limited_bulk_quote : public Bulk_quote {
+class Bulk_quote : public Disc_quote {
 	public:
+	Bulk_quote() = default;
+	Bulk_quote(const std::string &id, double sales_price, size_t qty, double dis) :
+		Disc_quote(id, sales_price, qty, dis) {}
+	
+	double net_price(size_t n) const override { 
+		if(n >= min_qty)
+			return n * price * (1 - discount);
+		else return n*price;
+	}
+};
+
+class Limited_bulk_quote : public Disc_quote {
+	public:
+	Limited_bulk_quote() = default;
 	Limited_bulk_quote(const std::string &id, double sales_price, size_t min_qty_, size_t max_qty_, double dis) :
-		Bulk_quote(id, sales_price, min_qty_, dis), max_qty(max_qty_) {}
+		Disc_quote(id, sales_price, min_qty_, dis), max_qty(max_qty_) {}
 	
 	double net_price(size_t n) const override {
 		if(n >= max_qty)
-			return Bulk_quote::net_price(max_qty) + Quote::net_price(n - max_qty); //(n - max_qty) * price;
-		else return Bulk_quote::net_price(n);
+			return max_qty * (1 - discount) * price + (n - max_qty) * price;
+		else if(n >= min_qty) 
+			return n * (1 - discount) * price;
+		else 
+			return n*price;
 	}
 	
 	std::ostream& debug(std::ostream& os = std::cout) override final{
-		Bulk_quote::debug() << ", max_quantity: " << max_qty;
+		Disc_quote::debug() << ", max_quantity: " << max_qty;
 		return os;
 	}
 	
 	private:
-	size_t max_qty;
+	size_t max_qty = 0;
 	
 };
 
