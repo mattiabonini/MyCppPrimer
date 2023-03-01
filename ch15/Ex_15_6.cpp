819600ec1cb3ab1d1e0496ba2abf1bb8b23f8156
mattiabonini <boninimattia96@gmail.com>
2023-03-01 12:08:19 -0500

    Basket class

--- ch15/Ex_15_6.cpp
+++ ch15/Ex_15_6.cpp
@@ -1,10 +1,15 @@
 #include <iostream>
 #include <string>
+#include <set>
+#include <memory>
 
 class Quote {
 	public:
 	Quote() = default;
 	Quote(const std::string &id, double sales_price) : bookNo(id), price(sales_price) {}
+	
+	virtual Quote* clone() const & {return new Quote(*this);}
+	virtual Quote* clone() && {return new Quote(std::move(*this));}
 	
 	std::string isbn() const {return bookNo; }
 	
@@ -46,12 +51,10 @@ class Disc_quote : public Quote {
 
 class Bulk_quote : public Disc_quote {
 	public:
-	/*
-	Bulk_quote() = default;
-	Bulk_quote(const std::string &id, double sales_price, size_t qty, double dis) :
-		Disc_quote(id, sales_price, qty, dis) {}
-	*/
 	using Disc_quote::Disc_quote;
+	
+	Bulk_quote* clone() const & override {return new Bulk_quote(*this);}
+	Bulk_quote* clone() && override {return new Bulk_quote(std::move(*this));}
 	
 	double net_price(size_t n) const override { 
 		if(n >= min_qty)
@@ -65,6 +68,9 @@ class Limited_bulk_quote : public Disc_quote {
 	Limited_bulk_quote() = default;
 	Limited_bulk_quote(const std::string &id, double sales_price, size_t min_qty_, size_t max_qty_, double dis) :
 		Disc_quote(id, sales_price, min_qty_, dis), max_qty(max_qty_) {}
+		
+	Limited_bulk_quote* clone() const & override {return new Limited_bulk_quote(*this);}
+	Limited_bulk_quote* clone() && override {return new Limited_bulk_quote(std::move(*this));}
 	
 	double net_price(size_t n) const override {
 		if(n >= max_qty)
@@ -85,11 +91,37 @@ class Limited_bulk_quote : public Disc_quote {
 	
 };
 
-void print(std::ostream& os, const Quote &q, size_t n) {
+class Basket {
+	public:
+	void add_item(const Quote &q) { items.insert(std::shared_ptr<Quote>(q.clone())); }
+	void add_item(Quote &&q) {items.insert(std::shared_ptr<Quote>(std::move(q).clone())); }
+	
+	double total_receipt(std::ostream& os = std::cout) const;
+	
+	private:
+	static bool compare(const std::shared_ptr<Quote> &lhs, const std::shared_ptr<Quote> &rhs) {
+		return lhs->isbn() < rhs->isbn();
+	}
+	
+	std::multiset<std::shared_ptr<Quote>, decltype(compare)*> items{compare};
+};
+
+double print(std::ostream& os, const Quote &q, size_t n) {
+	double ret = q.net_price(n);
 	os << "You purchased " << n << " copies of \"" << q.isbn() << 
-		"\" and the total price is " << q.net_price(n) << std::endl;
+		"\" and the total price is " << ret << std::endl;
+	return ret;
+	
 }
 
+double Basket::total_receipt(std::ostream& os) const {
+	double sum = 0.0;
+	for(auto iter = items.cbegin(); iter != items.cend(); iter = items.upper_bound(*iter))
+		sum += print(os, **iter, items.count(*iter));
+	os << "Total: " << sum << std::endl;
+	return sum;
+}	
+	
 int main() {
 	Quote q1("Harry Potter", 12);
 	Bulk_quote b1("Lord of the rings", 16, 5, .5);
@@ -104,6 +136,19 @@ int main() {
 	q1.debug() << std::endl;
 	b1.debug() << std::endl;
 	lb1.debug() << std::endl;
+	
+	Basket bsk;
+	bsk.add_item(q1);
+	bsk.add_item(b1);
+	bsk.add_item(b1);
+	bsk.add_item(b1);
+	bsk.add_item(lb1);
+	bsk.add_item(lb1);
+	bsk.add_item(lb1);
+	bsk.add_item(b1);
+	bsk.add_item(b1);
+	
+	bsk.total_receipt();
 	
 	return 0;
 }
